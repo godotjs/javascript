6ca717015fc607ee38d90d0470be5db57188a654
zw7u9tru <zw7u9tru@github>
2022-04-07 22:26:43 -0700

    upfinaltag

--- build_github_actions.py
+++ build_github_actions.py
@@ -221,47 +221,47 @@ var expected_to_see = """
         + str(len(wf_name_list))
         + """;
 while (total_slept < 3600000 && seen_completed_wfs.length < expected_to_see) {
-	var all_workflows = await github.rest.actions.listWorkflowRunsForRepo({
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-	});
-	console.log("Expecting to download from " + expected_to_see + " workflows, currently at " + seen_completed_wfs.length ". Have already downloaded " + downloaded_files.length + " files as " + downloaded_files);
-	for (const workflow of all_workflows.data.workflow_runs) {
-		if (workflow.head_sha == "${{ github.sha }}") {
-			console.log("found " + workflow.name + " " + workflow.status);
-			if (workflow.status == "completed") {
-				if (seen_completed_wfs.includes(workflow.name)) {continue;}
-				if (workflow.conclusion == "success") {
-					var artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-						owner: context.repo.owner,
-						repo: context.repo.repo,
-						run_id: workflow.id,
-						per_page: 100,
-					});
-					for (const artifact of artifacts.data.artifacts) {
-						var fn = '${{github.workspace}}/' + artifact.name + '.zip';
-						if (downloaded_files.includes(fn)) {continue;}
-						var download = await github.rest.actions.downloadArtifact({
-							owner: context.repo.owner,
-							repo: context.repo.repo,
-							artifact_id: artifact.id,
-							archive_format: 'zip',
-						});
-						var fs = require('fs');
-						fs.writeFileSync(fn, Buffer.from(download.data));
-						downloaded_files.push(fn);
-					}
-					seen_completed_wfs.push(workflow.name);
-				}
-			}
-		}
-	}
-	if (seen_completed_wfs.length < expected_to_see) {
-		console.log("sleeping " + 300000);
-		await new Promise(r => setTimeout(r, 300000));
-		total_slept = total_slept + 300000;
-		console.log("done sleeping " + 300000);
-	}
+    var all_workflows = await github.rest.actions.listWorkflowRunsForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    });
+    console.log("Expecting to download from " + expected_to_see + " workflows, currently at " + seen_completed_wfs.length + ". Have already downloaded " + downloaded_files.length + " files as " + downloaded_files);
+    for (const workflow of all_workflows.data.workflow_runs) {
+        if (workflow.head_sha == "${{ github.sha }}") {
+            console.log("found " + workflow.name + " " + workflow.status);
+            if (workflow.status == "completed") {
+                if (seen_completed_wfs.includes(workflow.name)) {continue;}
+                if (workflow.conclusion == "success") {
+                    var artifacts = await github.rest.actions.listWorkflowRunArtifacts({
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        run_id: workflow.id,
+                        per_page: 100,
+                    });
+                    for (const artifact of artifacts.data.artifacts) {
+                        var fn = '${{github.workspace}}/' + artifact.name + '.zip';
+                        if (downloaded_files.includes(fn)) {continue;}
+                        var download = await github.rest.actions.downloadArtifact({
+                            owner: context.repo.owner,
+                            repo: context.repo.repo,
+                            artifact_id: artifact.id,
+                            archive_format: 'zip',
+                        });
+                        var fs = require('fs');
+                        fs.writeFileSync(fn, Buffer.from(download.data));
+                        downloaded_files.push(fn);
+                    }
+                    seen_completed_wfs.push(workflow.name);
+                }
+            }
+        }
+    }
+    if (seen_completed_wfs.length < expected_to_see) {
+        console.log("sleeping " + 300000);
+        await new Promise(r => setTimeout(r, 300000));
+        total_slept = total_slept + 300000;
+        console.log("done sleeping " + 300000);
+    }
 }
 console.log(downloaded_files);"""
     )
