c59311e4f3c2d9a6ebf71ad17a9f984c87356ee5
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-09-27 23:03:10 +0200

    chore: removed generating typescript project

--- misc/generate/editor_tools.py
+++ misc/generate/editor_tools.py
@@ -12,17 +12,5 @@ def get_editor_tools_files():
         "editor/godot.d.ts.gen.cpp": (
             "BUILTIN_DECLARATION_TEXT",
             "misc/typescript/godot.d.ts",
-        ),
-        "editor/tsconfig.json.gen.cpp": (
-            "TSCONFIG_CONTENT",
-            "misc/typescript/tsconfig.json",
-        ),
-        "editor/decorators.ts.gen.cpp": (
-            "TS_DECORATORS_CONTENT",
-            "misc/typescript/decorators.ts",
-        ),
-        "editor/package.json.gen.cpp": (
-            "PACKAGE_JSON_CONTENT",
-            "misc/typescript/package.json",
-        ),
+        )
     }
